3c20fe973b494c9e4f603d9dd1f4c7828f6af014
argus96 <magar.ayush.010@gmail.com>
2025-02-02 14:14:13 +0545

    first commit

---
+++ assets/js/hero.js
@@ -0,0 +1,13 @@
+
+const swiper = new Swiper(".heroSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+  });
+  
